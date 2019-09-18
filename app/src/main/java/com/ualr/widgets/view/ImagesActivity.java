50698243715a892dd12ddef17966312d87ba241f
irconde <ivanrodriguezconde@gmail.com>
2019-09-17 23:44:28 -0500

    Added README file

---
+++ app/src/main/java/com/ualr/widgets/view/ImagesActivity.java
@@ -0,0 +1,19 @@
+package com.ualr.widgets.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ualr.widgets.R;
+
+/**
+ * Created by irconde on 2019-09-20.
+ */
+public class ImagesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+    }
+}
