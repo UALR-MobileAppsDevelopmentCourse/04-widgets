824304f554d5576630c92a83ae4d636ee80e345a
irconde <ivanrodriguezconde@gmail.com>
2019-09-18 12:20:50 -0500

    Added README file

---
+++ app/src/main/java/com/ualr/widgets/view/ArticleActivity.java
@@ -0,0 +1,17 @@
+package com.ualr.widgets.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ualr.widgets.R;
+
+public class ArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+    }
+
+}
